f9c01703523cea653c7f81cda5b78e5c436f03d3
Gian Carnero <gian.carnero@gmail.com>
2022-12-08 20:26:09 -0600

    removed code not needed

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,7 @@
 const deleteText = document.querySelectorAll('.fa-trash')
-const thumbText = document.querySelectorAll('.fa-thumbs-up')
 
 Array.from(deleteText).forEach((element)=>{
     element.addEventListener('click', deleteEntry)
-})
-
-Array.from(thumbText).forEach((element)=>{
-    element.addEventListener('click', addLike)
 })
 
 async function deleteEntry(){
